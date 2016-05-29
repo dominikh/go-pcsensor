e52b10734f28bce83ef2c9299f6cca83ee56e88b
Dominik Honnef <dominik@honnef.co>
2016-05-29 17:12:32 +0200

    Loop in command, printing temperatures every second

--- cmd/pcsensor/pcsensor.go
+++ cmd/pcsensor/pcsensor.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"honnef.co/go/pcsensor"
 
@@ -14,8 +18,21 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGTERM, syscall.SIGINT)
+	go func() {
+		<-sig
+		for _, sensor := range sensors {
+			_ = sensor.Close()
+		}
+		os.Exit(1)
+	}()
+
 	for _, sensor := range sensors {
-		defer sensor.Close()
-		log.Println(sensor.Temperatures())
+		for {
+			log.Println(sensor.Temperatures())
+			time.Sleep(1 * time.Second)
+		}
 	}
 }
