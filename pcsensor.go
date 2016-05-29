e718ae6ba875d3e5585ee9e8b3301275d57ede5c
Dominik Honnef <dominik@honnef.co>
2016-05-29 16:02:45 +0200

    Restructure code to support multiple device types
    In the future we may want to support more than one device type. For that
    reason, we're turning Sensor into an interface. Temperatures will be
    returned as a map, as some devices return only one temperature, some
    return two, and some return a whole array.

--- pcsensor.go
+++ pcsensor.go
@@ -15,9 +15,77 @@ const (
 	reqIntLen = 8
 )
 
-type Sensor struct {
+type Sensor interface {
+	Temperatures() (Temperatures, error)
+	Close() error
+}
+
+type Temperatures map[string]float64
+
+type temperv2 struct {
 	dev *usb.Device
 	ep  usb.Endpoint
+}
+
+func (s *temperv2) init() error {
+	const interface1 = 0x00
+	const interface2 = 0x01
+	_, err := s.dev.OpenEndpoint(0x01, interface1, 0, 0x81)
+	if err != nil {
+		return fmt.Errorf("error opening endpoint 1: %s", err)
+	}
+	endpoint2, err := s.dev.OpenEndpoint(0x01, interface2, 0, 0x82)
+	if err != nil {
+		return fmt.Errorf("error opening endpoint 2: %s", err)
+	}
+	s.ep = endpoint2
+
+	_, err = s.dev.Control(0x21, 0x09, 0x0201, interface1, []byte{0x01, 0x01})
+	if err != nil {
+		return fmt.Errorf("error communicating with sensor: %s", err)
+	}
+
+	uTemperature := []byte{0x01, 0x80, 0x33, 0x01, 0x00, 0x00, 0x00, 0x00}
+	err = control(s.dev, uTemperature)
+	if err != nil {
+		return fmt.Errorf("error communicating with sensor: %s", err)
+	}
+	b := make([]byte, reqIntLen)
+	_, err = endpoint2.Read(b)
+	if err != nil {
+		return fmt.Errorf("error reading from endpoint: %s", err)
+	}
+
+	uIni1 := []byte{0x01, 0x82, 0x77, 0x01, 0x00, 0x00, 0x00, 0x00}
+	err = control(s.dev, uIni1)
+	if err != nil {
+		return fmt.Errorf("error communicating with sensor: %s", err)
+	}
+	_, err = endpoint2.Read(b)
+	if err != nil {
+		return fmt.Errorf("error reading from endpoint: %s", err)
+	}
+
+	uIni2 := []byte{0x01, 0x86, 0xff, 0x01, 0x00, 0x00, 0x00, 0x00}
+	err = control(s.dev, uIni2)
+	if err != nil {
+		return fmt.Errorf("error communicating with sensor: %s", err)
+	}
+	_, err = endpoint2.Read(b)
+	if err != nil {
+		return fmt.Errorf("error reading from endpoint: %s", err)
+	}
+	_, err = endpoint2.Read(b)
+	if err != nil {
+		return fmt.Errorf("error reading from endpoint: %s", err)
+	}
+
+	err = control(s.dev, uTemperature)
+	if err != nil {
+		return fmt.Errorf("error communicating with sensor: %s", err)
+	}
+
+	return nil
 }
 
 func control(dev *usb.Device, msg []byte) error {
@@ -25,92 +93,40 @@ func control(dev *usb.Device, msg []byte) error {
 	return err
 }
 
-func New(ctx *usb.Context) (sensor *Sensor, err error) {
+func New(ctx *usb.Context) (Sensor, error) {
+	// TODO find all sensors, not just the first
 	dev, err := ctx.OpenDeviceWithVidPid(idVendor, idProduct)
 	if err != nil {
 		return nil, err
 	}
-
-	defer func() {
-		if err != nil {
-			_ = dev.Close()
-		}
-	}()
-
-	const interface1 = 0x00
-	const interface2 = 0x01
-	_, err = dev.OpenEndpoint(0x01, interface1, 0, 0x81)
-	if err != nil {
-		return nil, fmt.Errorf("error opening endpoint 1: %s", err)
+	sensor := &temperv2{dev: dev}
+	if err = sensor.init(); err != nil {
+		sensor.Close()
+		return nil, err
 	}
-	endpoint2, err := dev.OpenEndpoint(0x01, interface2, 0, 0x82)
-	if err != nil {
-		return nil, fmt.Errorf("error opening endpoint 2: %s", err)
-	}
-
-	_, err = dev.Control(0x21, 0x09, 0x0201, interface1, []byte{0x01, 0x01})
-	if err != nil {
-		return nil, fmt.Errorf("error communicating with sensor: %s", err)
-	}
-
-	uTemperature := []byte{0x01, 0x80, 0x33, 0x01, 0x00, 0x00, 0x00, 0x00}
-	err = control(dev, uTemperature)
-	if err != nil {
-		return nil, fmt.Errorf("error communicating with sensor: %s", err)
-	}
-	b := make([]byte, reqIntLen)
-	_, err = endpoint2.Read(b)
-	if err != nil {
-		return nil, fmt.Errorf("error reading from endpoint: %s", err)
-	}
-
-	uIni1 := []byte{0x01, 0x82, 0x77, 0x01, 0x00, 0x00, 0x00, 0x00}
-	err = control(dev, uIni1)
-	if err != nil {
-		return nil, fmt.Errorf("error communicating with sensor: %s", err)
-	}
-	_, err = endpoint2.Read(b)
-	if err != nil {
-		return nil, fmt.Errorf("error reading from endpoint: %s", err)
-	}
-
-	uIni2 := []byte{0x01, 0x86, 0xff, 0x01, 0x00, 0x00, 0x00, 0x00}
-	err = control(dev, uIni2)
-	if err != nil {
-		return nil, fmt.Errorf("error communicating with sensor: %s", err)
-	}
-	_, err = endpoint2.Read(b)
-	if err != nil {
-		return nil, fmt.Errorf("error reading from endpoint: %s", err)
-	}
-	_, err = endpoint2.Read(b)
-	if err != nil {
-		return nil, fmt.Errorf("error reading from endpoint: %s", err)
-	}
-
-	err = control(dev, uTemperature)
-	if err != nil {
-		return nil, fmt.Errorf("error communicating with sensor: %s", err)
-	}
-
-	return &Sensor{dev, endpoint2}, nil
+	return sensor, nil
 }
 
-func (s *Sensor) Close() error {
+func (s *temperv2) Close() error {
 	return s.dev.Close()
 }
 
-func (s *Sensor) Temperatures() (inner, outer float64, err error) {
+func (s *temperv2) Temperatures() (temps Temperatures, err error) {
 	b := make([]byte, reqIntLen)
 	_, err = s.ep.Read(b)
 	if err != nil {
-		return 0, 0, fmt.Errorf("error reading from endpoint: %s", err)
+		return nil, fmt.Errorf("error reading from endpoint: %s", err)
 	}
+	// TODO look up a spec sheet of the sensor to make sense of these
+	// values
 	temp := int16(b[3]&0xFF) + (int16(b[2]) << 8)
-	inner = float64(temp) * (125.0 / 32000.0)
+	inner := float64(temp) * (125.0 / 32000.0)
 
 	temp = int16(b[5]&0xFF) + (int16(b[4]) << 8)
-	outer = float64(temp) * (125.0 / 32000.0)
+	outer := float64(temp) * (125.0 / 32000.0)
 
-	return inner, outer, nil
+	return Temperatures{
+		"inner": inner,
+		"outer": outer,
+	}, nil
 }
